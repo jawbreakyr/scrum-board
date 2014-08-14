7e68797bd84e90212c75bf077b0790f3436af01d
Aldrin Navarro <aldrinnavarro16@gmail.com>
2014-08-14 10:45:18 +0800

    Added task model

--- task/models.py
+++ task/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=200, null=True, blank=True)
+    body = models.TextField()
+    STORY = 'ST'
+    TODO = 'TD'
+    IN_PROGRESS = 'IP'
+    TESTING = 'TE'
+    DONE = 'DN'
+    STATUS_CHOICES = (
+        (STORY, 'Stories'),
+        (TODO, 'To do'),
+        (IN_PROGRESS, 'In Progress'),
+        (TESTING, 'Testing'),
+        (DONE, 'Done')
+    )
+    status = models.CharField(
+        max_length=2, choices=STATUS_CHOICES, default=STORY)
+    owner = models.OneToOneField(User, related_name="owned_tasks")
+    moderator = models.ForeignKey(User, related_name="moderated_tasks")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.title
