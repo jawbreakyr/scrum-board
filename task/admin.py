148640b3aef9aef43e912240e5ffc52fc42a7f7c
Aldrin Navarro <aldrinnavarro16@gmail.com>
2014-08-14 10:45:05 +0800

    Added taskadmin

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from task.models import Task
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        '__unicode__', 'body', 'owner', 'moderator', 'status',
+        'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('title', 'body', 'owner__username', 'moderator__username')
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = [
+        ('Story', {
+            'fields': ('title', 'body', 'status')
+        }),
+        ('Auth', {
+            'classes': ('collapse',),
+            'fields': ('owner', 'moderator')
+        }),
+        ('Change History', {
+            'classes': ('collapse',),
+            'fields': ('updated_at', 'created_at')
+        })
+    ]
+
+admin.site.register(Task, TaskAdmin)
