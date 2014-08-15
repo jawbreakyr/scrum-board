7b88f9cc8abdaafc8d71d47c59c8b7f09099fed4
Aldrin Navarro <aldrinnavarro16@gmail.com>
2014-08-15 17:40:44 +0800

    merged conflicts for views

--- task/views.py
+++ task/views.py
@@ -6,25 +6,22 @@ from task.models import Task
 
 
 # def home(request):
-# 	tasks = Task.objects.all()
-# 	form = TaskForm()
-# 	c = {'form': form, 'tasks': tasks }
-# 	return render(request, 'task/home.html', c)
+#   tasks = Task.objects.all()
+#   form = TaskForm()
+#   c = {'form': form, 'tasks': tasks }
+#   return render(request, 'task/home.html', c)
 
 
 # Implementing the use of generic class based views
 class PublisherView(generic.ListView):
-	template_name = 'task/home.html'
-	# context_object_name = "tasks"
-	queryset = Task.objects.all()
+    template_name = 'task/home.html'
+    # context_object_name = "tasks"
+    queryset = Task.objects.all()
 
-	def get_context_data(self, **kwargs):
-		context = super(PublisherView, self).get_context_data(**kwargs)
-		# appended the objects to the context dictionary (force)
-		context['form'] = TaskForm()
-		# appended the objects to the context dictionary (force)
-		context['tasks'] = Task.objects.all()
-		return context
-
-
-
+    def get_context_data(self, **kwargs):
+        context = super(PublisherView, self).get_context_data(**kwargs)
+        # appended the objects to the context dictionary (force)
+        context['form'] = TaskForm()
+        # appended the objects to the context dictionary (force)
+        context['tasks'] = Task.objects.all()
+        return context
