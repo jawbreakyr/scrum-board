623b8b65fc8826ee11b74792432247ea79532883
hubert <jawbreakyr@gmail.com>
2014-08-15 14:42:30 +0800

    Insisted the class base view :D

--- scrum_board/urls.py
+++ scrum_board/urls.py
@@ -3,13 +3,19 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from task import views
+# from task.views import HomeView
+from task.models import Task
+from task.views import PublisherView
 
 urlpatterns = patterns('',
+	url(r'^$', PublisherView.as_view()),
+		# queryset = Task.objects.all(),
+		# context_object_name="tasks",
+		# )),
     # Examples:
     # url(r'^$', 'scrum_board.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    #url(r'^$', HomeView.as_view()),
-    url(r'^$', views.home, name='home'),
+    # url(r'^$', HomeView.as_view()),
+    # url(r'^$', views.home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- task/views.py
+++ task/views.py
@@ -1,19 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.views import generic
 
 from task.forms import TaskForm
 from task.models import Task
 
 
-def home(request):
-	tasks = Task.objects.all()
-	form = TaskForm()
-	c = {'form': form, 'tasks': tasks }
-	return render(request, 'task/home.html', c)
+# def home(request):
+# 	tasks = Task.objects.all()
+# 	form = TaskForm()
+# 	c = {'form': form, 'tasks': tasks }
+# 	return render(request, 'task/home.html', c)
 
-# class HomeView(generic.TemplateView):
-# 	template_name = 'task/home.html'
 
-# 	def get(self, request, *args, **kwargs):
-# 		context = {"form": TaskForm()}
-# 		return self.render_to_response(context)
+# Implementing the use of generic class based views
+class PublisherView(generic.ListView):
+	template_name = 'task/home.html'
+	# context_object_name = "tasks"
+	queryset = Task.objects.all()
+
+	def get_context_data(self, **kwargs):
+		context = super(PublisherView, self).get_context_data(**kwargs)
+		# appended the objects to the context dictionary (force)
+		context['form'] = TaskForm()
+		# appended the objects to the context dictionary (force)
+		context['tasks'] = Task.objects.all()
+		return context
+
+
+
